662850f2ff71240295219f4a6523cabc3ee977bc
Nazakat341 <nazakatshehzad341@gmail.com>
2021-10-07 07:51:49 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const connectDB = require("./config/db");
+const path = require("path");
+
+const app = express();
+
+connectDB();
+
+
+
+// Init Middelware
+app.use(express.json({ extended: false }));
+
+// Defineing Routes
+app.use('/api/users',require('./routes/users'));
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/contacts',require('./routes/contacts'));
+
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
